3f1571adb52581b96c9a57e13cd35e0199d5aea0
ioan <gitlabjoan@gmail.com>
2020-12-03 10:19:43 +0100

    modified updated

--- routers/user.js
+++ routers/user.js
@@ -49,7 +49,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findByIdAndUpdate(req.params.id)
+       updates.forEach((update) => {
+            users[update]= req.body[update];
+       })
+       user.save();
 
         if (!user) {
             return res.status(404).send()
